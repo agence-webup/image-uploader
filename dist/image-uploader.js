9f368a889167e902026d4204d02fdf9d4a0f4542
Baptiste DUCATEL <baptiste@agence-webup.com>
2016-12-06 14:52:03 +0100

    refactor image uploader class

--- dist/image-uploader.js
+++ dist/image-uploader.js
@@ -56,8 +56,49 @@ var CropperModal = function () {
 
 "use strict";
 
+/*
+ * ImageUploader class
+ */
 var ImageUploader = function () {
-    function ImageUploader(el) {
+
+    /* private properties
+    -------------------------------------------------------------- */
+
+    /**
+     * service adapter
+     * @type {Service}
+     */
+    var _service = Symbol();
+
+    /**
+     * picture list
+     * @type {Array}
+     */
+    var _pictures = Symbol();
+
+    /**
+     * picture view list
+     * @type {Array}
+     */
+    var _pictureViews = Symbol();
+
+    /**
+     * index of current edited picture
+     * @type {?int}
+     */
+    var _editIndex = Symbol();
+
+    /**
+     * options
+     * @type {Object}
+     */
+    var _options = Symbol();
+
+    /**
+     * ImageUploader
+     */
+
+    var ImageUploader = function ImageUploader(el) {
         var _this2 = this;
 
         var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
@@ -66,158 +107,202 @@ var ImageUploader = function () {
 
         this.el = el;
         var defaults = {
-            cropper: false
+            cropper: false,
+            service: null
         };
-        this.options = Object.assign({}, defaults, options);
-        this._pictures = [];
-        this._service = new ServiceMock();
+        this[_options] = Object.assign({}, defaults, options);
+        this[_pictures] = [];
+        this[_pictureViews] = [];
+        this[_service] = this[_options].service == null ? new ServiceMock() : this[_options].service;
+        this[_editIndex] = null;
 
-        this._service.all().then(function (pictures) {
-            _this2._pictures = pictures;
-            _this2.reloadView();
+        this[_service].all().then(function (pictures) {
+            _this2[_pictures] = pictures;
+            initView.bind(_this2)();
         });
-        this._currentId = null;
+    };
+
+    /* private methods
+    -------------------------------------------------------------- */
+
+    /**
+     * Add a picture
+     * @param {File} file
+     * @param {?Object} crop
+     */
+
+
+    function addPicture(file, crop) {
+        var _this3 = this;
+
+        var pictureDto = {
+            file: file,
+            crop: crop
+        };
+        this[_service].add(pictureDto).then(function (picture) {
+            var length = _this3[_pictureViews].length;
+            var view = makePictureView.bind(_this3)(picture, length);
+
+            _this3.el.insertBefore(view, _this3[_pictureViews][length - 1].nextSibling);
+
+            _this3[_pictures].push(picture);
+            _this3[_pictureViews].push(view);
+        });
     }
 
-    _createClass(ImageUploader, [{
-        key: 'uploadFile',
-        value: function uploadFile(event) {
-            var _this3 = this;
+    /**
+     * Update the picture at index
+     * @param  {int} index
+     * @param  {File} file
+     * @param  {?Object} crop
+     */
+    function updatePicture(index, file, crop) {
+        var _this4 = this;
 
-            var fileInput = event.target;
-            if (fileInput.files.length == 0) {
-                return;
-            }
-            var file = fileInput.files[0];
+        var pictureDto = {
+            id: this[_pictures][index].id,
+            file: file,
+            crop: crop
+        };
+        this[_service].update(pictureDto).then(function (picture) {
+            _this4[_pictureViews][index].querySelector('img').setAttribute('src', picture.url);
+            _this4[_pictures][index] = picture;
+        });
+    }
 
-            if (this.options.cropper) {
-                this.modal = new CropperModal(file, function (data) {
-                    if (_this3._currentId == null) {
-                        _this3.addPicture(file, data);
-                    } else {
-                        _this3.updatePicture(_this3._currentId, file, data);
-                        _this3._currentId = null;
-                    }
-                });
-            } else {
-                if (this._currentId == null) {
-                    this.addPicture(file);
-                } else {
-                    this.updatePicture(this._currentId, file);
-                    this._currentId = null;
-                }
-            }
+    /**
+     * Remove the picture at index
+     * @param  {int} index
+     */
+    function removePicture(index) {
+        var _this5 = this;
+
+        var picture = this[_pictures][index];
+        this[_service].delete(picture.id).then(function () {
+            _this5.el.removeChild(_this5[_pictureViews][index]);
+            _this5[_pictures].splice(index, 1);
+            _this5[_pictureViews].splice(index, 1);
+        });
+    }
+
+    /**
+     * Select file Handler
+     * @param  Event event
+     */
+    function selectFile(event) {
+        var _this6 = this;
+
+        var fileInput = event.target;
+        if (fileInput.files.length == 0) {
+            return;
         }
-    }, {
-        key: 'addPicture',
-        value: function addPicture(file, crop) {
-            var _this4 = this;
+        var file = fileInput.files[0];
 
-            var pictureDto = {
-                file: file,
-                crop: crop
-            };
-            this._service.add(pictureDto).then(function (picture) {
-                _this4._pictures.push(picture);
-                _this4.reloadView();
+        if (this[_options].cropper) {
+            this.modal = new CropperModal(file, function (data) {
+                uploadFile.bind(_this6)(_this6[_editIndex], file, data);
+                _this6[_editIndex] = null;
             });
+        } else {
+            uploadFile.bind(this)(this[_editIndex], file);
+            this[_editIndex] = null;
         }
-    }, {
-        key: 'updatePicture',
-        value: function updatePicture(id, file, crop) {
-            var _this5 = this;
+    }
 
-            var pictureDto = {
-                id: id,
-                file: file,
-                crop: crop
-            };
-            this._service.update(pictureDto).then(function (picture) {
-                _this5.reloadView();
-            });
+    /**
+     * Add or update picture
+     * @param  {?int} index if null add else update
+     * @param  {File} file
+     * @param  {?Object} crop
+     */
+    function uploadFile(index, file, crop) {
+        if (index === null) {
+            addPicture.bind(this)(file, crop);
+        } else {
+            updatePicture.bind(this)(index, file, crop);
         }
+    }
 
-        /* view
-        -------------------------------------------------------------- */
+    /**
+     * Init view
+     */
+    function initView() {
+        var _this7 = this;
 
-    }, {
-        key: 'reloadView',
-        value: function reloadView() {
-            var _this6 = this;
+        this[_pictures].forEach(function (picture, index) {
+            var view = makePictureView.bind(_this7)(picture, index);
+            _this7.el.appendChild(view);
+            _this7[_pictureViews].push(view);
+        });
 
-            while (this.el.firstChild) {
-                this.el.removeChild(this.el.firstChild);
-            }
+        initAddView.bind(this)();
+    }
 
-            this._pictures.forEach(function (picture) {
-                var div = document.createElement('div');
-                var img = createElement('img', {
-                    src: picture.url
-                });
+    function makePictureView(picture, index) {
+        var div = document.createElement('div');
+        var img = createElement('img', {
+            src: picture.url
+        });
 
-                var span = createElement('span', {
-                    class: 'dropmic',
-                    'data-dropmic': picture.id,
-                    'data-dropmic-direction': 'bottom-right'
-                });
+        var span = createElement('span', {
+            class: 'dropmic',
+            'data-dropmic': picture.id,
+            'data-dropmic-direction': 'bottom-right'
+        });
 
-                var button = createElement('button', {
-                    'data-dropmic-btn': null
-                });
-                button.innerHTML = 'Actions';
-                span.appendChild(button);
-                div.appendChild(span);
+        var button = createElement('button', {
+            'data-dropmic-btn': null
+        });
+        button.innerHTML = 'Actions';
+        span.appendChild(button);
+        div.appendChild(span);
 
-                var dropmic = new Dropmic(span);
-                dropmic.addBtn('Modifier', function () {
-                    _this6._currentId = dropmic.target.getAttribute('data-dropmic');
-                    _this6._fileInput.click();
-                });
+        initDopmic.bind(this)(span, index);
 
-                dropmic.addBtn('Supprimer', function () {
-                    _this6._currentId = null;
-                    var id = dropmic.target.getAttribute('data-dropmic');
-                    _this6._service.delete(id).then(function () {
-                        _this6._service.all().then(function (pictures) {
-                            _this6._pictures = pictures;
-                            _this6.reloadView();
-                        });
-                    });
-                });
+        div.appendChild(img);
 
-                div.appendChild(img);
+        return div;
+    }
 
-                _this6.el.appendChild(div);
-            });
+    function initDopmic(el, index) {
+        var _this8 = this;
 
-            this.initAddView();
-        }
-    }, {
-        key: 'initAddView',
-        value: function initAddView() {
-            var _this7 = this;
+        var dropmic = new Dropmic(el);
 
-            this._fileInput = createElement('input', {
-                type: 'file'
-            });
-            this._fileInput.addEventListener('change', function (event) {
-                _this7.uploadFile(event);
-            });
+        dropmic.addBtn('Modifier', function () {
+            _this8[_editIndex] = index;
+            _this8._fileInput.click();
+        });
 
-            var div = document.createElement('div');
-            div.addEventListener('click', function (event) {
-                _this7._fileInput.click();
-            });
+        dropmic.addBtn('Supprimer', function () {
+            removePicture.bind(_this8)(index);
+        });
+    }
 
-            div.appendChild(this._fileInput);
+    /**
+     * Init add View
+     */
+    function initAddView() {
+        var _this9 = this;
 
-            var icon = document.createElement('i');
-            icon.innerHTML = 'add icon';
-            div.appendChild(icon);
+        this._fileInput = createElement('input', {
+            type: 'file'
+        });
+        this._fileInput.addEventListener('change', selectFile.bind(this));
 
-            this.el.appendChild(div);
-        }
-    }]);
+        var div = document.createElement('div');
+        div.addEventListener('click', function (event) {
+            _this9._fileInput.click();
+        });
+
+        div.appendChild(this._fileInput);
+
+        var icon = document.createElement('i');
+        icon.innerHTML = 'add icon';
+        div.appendChild(icon);
+
+        this.el.appendChild(div);
+    }
 
     return ImageUploader;
 }();
@@ -254,26 +339,26 @@ var ServiceMock = function () {
     _createClass(ServiceMock, [{
         key: 'all',
         value: function all() {
-            var _this8 = this;
+            var _this10 = this;
 
             return new Promise(function (resolve, reject) {
-                resolve(_this8._pictures);
+                resolve(_this10._pictures);
             });
         }
     }, {
         key: 'add',
         value: function add(pictureDto) {
-            var _this9 = this;
+            var _this11 = this;
 
             return new Promise(function (resolve, reject) {
                 var fileReader = new FileReader();
                 fileReader.addEventListener('load', function (event) {
                     var picture = {
-                        id: _this9._pictures.length + 1,
+                        id: _this11._pictures.length + 1,
                         url: event.target.result
                     };
 
-                    _this9._pictures.push(picture);
+                    _this11._pictures.push(picture);
                     resolve(picture);
                 });
                 fileReader.readAsDataURL(pictureDto.file);
@@ -282,15 +367,15 @@ var ServiceMock = function () {
     }, {
         key: 'update',
         value: function update(pictureDto) {
-            var _this10 = this;
+            var _this12 = this;
 
             return new Promise(function (resolve, reject) {
                 var fileReader = new FileReader();
                 fileReader.addEventListener('load', function (event) {
-                    for (var i = 0; i < _this10._pictures.length; i++) {
-                        var picture = _this10._pictures[i];
+                    for (var i = 0; i < _this12._pictures.length; i++) {
+                        var picture = _this12._pictures[i];
                         if (picture.id == pictureDto.id) {
-                            _this10._pictures[i].url = event.target.result;
+                            _this12._pictures[i].url = event.target.result;
                             resolve(picture);
                             return;
                         }
@@ -304,10 +389,10 @@ var ServiceMock = function () {
     }, {
         key: 'delete',
         value: function _delete(id, callback) {
-            var _this11 = this;
+            var _this13 = this;
 
             return new Promise(function (resolve, reject) {
-                _this11._pictures = _this11._pictures.filter(function (picture) {
+                _this13._pictures = _this13._pictures.filter(function (picture) {
                     return picture.id != id;
                 });
                 resolve();
--- src/image-uploader.js
+++ src/image-uploader.js
@@ -1,130 +1,217 @@
 "use strict";
 
-class ImageUploader {
+/*
+ * ImageUploader class
+ */
+const ImageUploader = (function() {
 
-    constructor(el, options = {}) {
-        this.el = el;
-        const defaults = {
-            cropper: false,
-        };
-        this.options = Object.assign({}, defaults, options);
-        this._pictures = [];
-        this._service = new ServiceMock();
+    /* private properties
+    -------------------------------------------------------------- */
 
-        this._service.all().then((pictures) => {
-            this._pictures = pictures;
-            this.reloadView();
-        });
-        this._currentId = null;
+    /**
+     * service adapter
+     * @type {Service}
+     */
+    let _service = Symbol();
+
+    /**
+     * picture list
+     * @type {Array}
+     */
+    let _pictures = Symbol();
+
+    /**
+     * picture view list
+     * @type {Array}
+     */
+    let _pictureViews = Symbol();
+
+    /**
+     * index of current edited picture
+     * @type {?int}
+     */
+    let _editIndex = Symbol();
+
+    /**
+     * options
+     * @type {Object}
+     */
+    let _options = Symbol();
+
+    /**
+     * ImageUploader
+     */
+    class ImageUploader {
+        constructor(el, options = {}) {
+            this.el = el;
+            const defaults = {
+                cropper: false,
+                service: null,
+            };
+            this[_options] = Object.assign({}, defaults, options);
+            this[_pictures] = [];
+            this[_pictureViews] = [];
+            this[_service] = this[_options].service == null ? new ServiceMock() : this[_options].service;
+            this[_editIndex] = null;
+
+            this[_service].all().then((pictures) => {
+                this[_pictures] = pictures;
+                initView.bind(this)();
+            });
+        }
     }
 
-    uploadFile(event) {
+    /* private methods
+    -------------------------------------------------------------- */
+
+    /**
+     * Add a picture
+     * @param {File} file
+     * @param {?Object} crop
+     */
+    function addPicture(file, crop) {
+        var pictureDto = {
+            file: file,
+            crop: crop,
+        }
+        this[_service].add(pictureDto).then((picture) => {
+            const length = this[_pictureViews].length;
+            const view = makePictureView.bind(this)(picture, length);
+
+            this.el.insertBefore(view, this[_pictureViews][length - 1].nextSibling);
+
+            this[_pictures].push(picture);
+            this[_pictureViews].push(view);
+        });
+    }
+
+    /**
+     * Update the picture at index
+     * @param  {int} index
+     * @param  {File} file
+     * @param  {?Object} crop
+     */
+    function updatePicture(index, file, crop) {
+        var pictureDto = {
+            id: this[_pictures][index].id,
+            file: file,
+            crop: crop,
+        }
+        this[_service].update(pictureDto).then((picture) => {
+            this[_pictureViews][index].querySelector('img').setAttribute('src', picture.url);
+            this[_pictures][index] = picture;
+        });
+    }
+
+    /**
+     * Remove the picture at index
+     * @param  {int} index
+     */
+    function removePicture(index) {
+        const picture = this[_pictures][index];
+        this[_service].delete(picture.id).then(() => {
+            this.el.removeChild(this[_pictureViews][index]);
+            this[_pictures].splice(index, 1);
+            this[_pictureViews].splice(index, 1);
+        });
+    }
+
+    /**
+     * Select file Handler
+     * @param  Event event
+     */
+    function selectFile(event) {
         const fileInput = event.target;
         if (fileInput.files.length == 0) {
             return;
         }
         const file = fileInput.files[0];
 
-        if (this.options.cropper) {
+        if (this[_options].cropper) {
             this.modal = new CropperModal(file, (data) => {
-                if (this._currentId == null) {
-                    this.addPicture(file, data);
-                } else {
-                    this.updatePicture(this._currentId, file, data);
-                    this._currentId = null;
-                }
+                uploadFile.bind(this)(this[_editIndex], file, data);
+                this[_editIndex] = null;
             });
         } else {
-            if (this._currentId == null) {
-                this.addPicture(file);
-            } else {
-                this.updatePicture(this._currentId, file);
-                this._currentId = null;
-            }
+            uploadFile.bind(this)(this[_editIndex], file);
+            this[_editIndex] = null;
         }
     }
 
-    addPicture(file, crop) {
-        var pictureDto = {
-            file: file,
-            crop: crop,
+    /**
+     * Add or update picture
+     * @param  {?int} index if null add else update
+     * @param  {File} file
+     * @param  {?Object} crop
+     */
+    function uploadFile(index, file, crop) {
+        if (index === null) {
+            addPicture.bind(this)(file, crop);
+        } else {
+            updatePicture.bind(this)(index, file, crop);
         }
-        this._service.add(pictureDto).then((picture) => {
-            this._pictures.push(picture);
-            this.reloadView();
+    }
+
+    /**
+     * Init view
+     */
+    function initView() {
+        this[_pictures].forEach((picture, index) => {
+            const view = makePictureView.bind(this)(picture, index);
+            this.el.appendChild(view);
+            this[_pictureViews].push(view);
+        });
+
+        initAddView.bind(this)();
+    }
+
+    function makePictureView(picture, index) {
+        let div = document.createElement('div');
+        let img = createElement('img', {
+            src: picture.url
+        });
+
+        let span = createElement('span', {
+            class: 'dropmic',
+            'data-dropmic': picture.id,
+            'data-dropmic-direction': 'bottom-right'
+        });
+
+        let button = createElement('button', {
+            'data-dropmic-btn': null
+        });
+        button.innerHTML = 'Actions';
+        span.appendChild(button);
+        div.appendChild(span);
+
+        initDopmic.bind(this)(span, index);
+
+        div.appendChild(img);
+
+        return div;
+    }
+
+    function initDopmic(el, index) {
+        let dropmic = new Dropmic(el);
+
+        dropmic.addBtn('Modifier', () => {
+            this[_editIndex] = index;
+            this._fileInput.click();
+        });
+
+        dropmic.addBtn('Supprimer', () => {
+            removePicture.bind(this)(index);
         });
     }
 
-    updatePicture(id, file, crop) {
-        var pictureDto = {
-            id: id,
-            file: file,
-            crop: crop,
-        }
-        this._service.update(pictureDto).then((picture) => {
-            this.reloadView();
-        });
-    }
-
-    /* view
-    -------------------------------------------------------------- */
-
-    reloadView() {
-        while (this.el.firstChild) {
-            this.el.removeChild(this.el.firstChild);
-        }
-
-        this._pictures.forEach((picture) => {
-            let div = document.createElement('div');
-            let img = createElement('img', {
-                src: picture.url
-            });
-
-            let span = createElement('span', {
-                class: 'dropmic',
-                'data-dropmic': picture.id,
-                'data-dropmic-direction': 'bottom-right'
-            });
-
-            let button = createElement('button', {
-                'data-dropmic-btn': null
-            });
-            button.innerHTML = 'Actions';
-            span.appendChild(button);
-            div.appendChild(span);
-
-            let dropmic = new Dropmic(span);
-            dropmic.addBtn('Modifier', () => {
-                this._currentId = dropmic.target.getAttribute('data-dropmic');
-                this._fileInput.click();
-            })
-
-            dropmic.addBtn('Supprimer', () => {
-                this._currentId = null;
-                var id = dropmic.target.getAttribute('data-dropmic');
-                this._service.delete(id).then(() => {
-                    this._service.all().then((pictures) => {
-                        this._pictures = pictures;
-                        this.reloadView();
-                    });
-                });
-            })
-
-            div.appendChild(img);
-
-            this.el.appendChild(div);
-        });
-
-        this.initAddView();
-    }
-
-    initAddView() {
+    /**
+     * Init add View
+     */
+    function initAddView() {
         this._fileInput = createElement('input', {
             type: 'file',
         });
-        this._fileInput.addEventListener('change', (event) => {
-            this.uploadFile(event);
-        });
+        this._fileInput.addEventListener('change', selectFile.bind(this));
 
         let div = document.createElement('div');
         div.addEventListener('click', (event) => {
@@ -139,7 +226,9 @@ class ImageUploader {
 
         this.el.appendChild(div);
     }
-}
+
+    return ImageUploader;
+})();
 
 /* helpers
 -------------------------------------------------------------- */
